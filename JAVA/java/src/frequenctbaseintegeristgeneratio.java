11bced317c360ba01f5b935b12d4f609e46b2341
Rohan Mallick <rohanmallick016@gmail.com>
2024-01-02 22:19:33 +0530

    Add files via upload

---
+++ JAVA/java/src/frequenctbaseintegeristgeneratio.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class frequenctbaseintegeristgeneratio{
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Example input array of integers
+        int[] nums = {1,2,3,4};
+
+        List<List<Integer>> result = solution.findMatrix(nums);
+
+        // Displaying the resulting matrix of integers
+        System.out.println("Matrix of Integers:");
+        for (List<Integer> row : result) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Solution {
+    public List<List<Integer>> findMatrix(int[] nums) {
+        int freq[] = new int[nums.length + 1];
+
+        ArrayList<List<Integer>> ans = new ArrayList<>();
+        for (int c : nums) {
+            if (freq[c] >= ans.size()) {
+                ans.add(new ArrayList<>());
+            }
+
+            // Store the integer in the list corresponding to its current frequency.
+            ans.get(freq[c]).add(c);
+            freq[c]++;
+        }
+
+        return ans;
+    }
+}
+
+
+/*
+Input: nums = [1,3,4,1,2,3,1]
+Output: [[1,3,4,2],[1,3],[1]]
+Explanation: We can create a 2D array that contains the following rows:
+- 1,3,4,2
+- 1,3
+- 1
+All elements of nums were used, and each row of the 2D array contains distinct integers, so it is a valid answer.
+It can be shown that we cannot have less than 3 rows in a valid array.
+
+
+
+Input: nums = [1,2,3,4]
+Output: [[4,3,2,1]]
+Explanation: All elements of the array are distinct, so we can keep all of them in the first row of the 2D array.
+
+*/
