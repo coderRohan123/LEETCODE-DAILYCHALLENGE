38ede745f118e32a8ed36798e6451b19351175a2
Rohan Mallick <rohanmallick016@gmail.com>
2024-01-25 20:36:10 +0530

    Add files via upload

---
+++ python/Longest Common Subsequence.py
@@ -0,0 +1,50 @@
+def longestCommonSubsequence(text1,text2):
+    # Initialize a 2D array with dimensions len(text1)+1 by len(text2)+1
+    dp = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+
+    # Fill the matrix based on the logic for longest common subsequence
+    for i in range(1, len(text1) + 1):
+        for j in range(1, len(text2) + 1):
+            if text1[i - 1] == text2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    # Return the length of the longest common subsequence
+    return dp[len(text1)][len(text2)]
+
+text1 = "abcde"
+text2= "ace"
+print(longestCommonSubsequence(text1,text2))
+
+'''
+    Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+
+ 
+
+Example 1:
+
+Input: text1 = "abcde", text2 = "ace" 
+Output: 3  
+Explanation: The longest common subsequence is "ace" and its length is 3.
+Example 2:
+
+Input: text1 = "abc", text2 = "abc"
+Output: 3
+Explanation: The longest common subsequence is "abc" and its length is 3.
+Example 3:
+
+Input: text1 = "abc", text2 = "def"
+Output: 0
+Explanation: There is no such common subsequence, so the result is 0.
+ 
+
+Constraints:
+
+1 <= text1.length, text2.length <= 1000
+text1 and text2 consist of only lowercase English characters.'''
