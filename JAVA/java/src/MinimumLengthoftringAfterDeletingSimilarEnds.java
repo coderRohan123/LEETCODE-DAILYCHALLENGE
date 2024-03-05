3cccffcfc2646cddd84f3a4d6864eaeaf8899ec8
Rohan Mallick <rohanmallick016@gmail.com>
2024-03-05 20:03:25 +0530

    Add files via upload

---
+++ JAVA/java/src/MinimumLengthoftringAfterDeletingSimilarEnds.java
@@ -0,0 +1,29 @@
+public class MinimumLengthoftringAfterDeletingSimilarEnds {
+    public int minimumLength(String s) {
+        int begin = 0;
+        int end = s.length() - 1;
+
+        // Delete similar ends until the ends differ or they meet in the middle
+        while (begin < end && s.charAt(begin) == s.charAt(end)) {
+            char c = s.charAt(begin);
+
+            // Delete consecutive occurrences of c from prefix
+            while (begin <= end && s.charAt(begin) == c) {
+                begin++;
+            }
+
+            // Delete consecutive occurrences of c from suffix
+            while (end > begin && s.charAt(end) == c) {
+                end--;
+            }
+        }
+
+        // Return the number of remaining characters
+        return end - begin + 1;
+    }
+
+    public static void main(String[] args) {
+        MinimumLengthoftringAfterDeletingSimilarEnds ob=new MinimumLengthoftringAfterDeletingSimilarEnds();
+        System.out.println(ob.minimumLength("ca"));
+    }
+}
