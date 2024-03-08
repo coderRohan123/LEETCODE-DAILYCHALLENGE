42a088c8635a778c7edf9e1b7b20555d04660eaf
Rohan Mallick <rohanmallick016@gmail.com>
2024-03-08 10:32:47 +0530

    Add files via upload

---
+++ JAVA/java/src/CountElementsWithMaximumFrequency.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountElementsWithMaximumFrequency {
+    public int maxFrequencyElements(int[] nums) {
+        Map<Integer, Integer> frequencies = new HashMap<>();
+        int maxFrequency = 0;
+        int totalFrequencies = 0;
+
+        // Find the frequency of each element
+        // Determine the maximum frequency
+        // Calculate the total frequencies of elements with the maximum frequency
+        for (int num : nums) {
+            frequencies.put(num, frequencies.getOrDefault(num, 0) + 1);
+            int frequency = frequencies.get(num);
+
+            // If we discover a higher frequency element
+            // Update maxFrequency
+            // Re-set totalFrequencies to the new max frequency
+            if (frequency > maxFrequency) {
+                maxFrequency = frequency;
+                totalFrequencies = frequency;
+                // If we find an element with the max frequency, add it to the total
+            } else if (frequency == maxFrequency) {
+                totalFrequencies += frequency;
+            }
+        }
+        return totalFrequencies;
+    }
+
+    public static void main(String[] args) {
+        CountElementsWithMaximumFrequency ob=new CountElementsWithMaximumFrequency();
+        System.out.println(ob.maxFrequencyElements(new int[] {1,2,2,3,1,4}));
+    }
+}
