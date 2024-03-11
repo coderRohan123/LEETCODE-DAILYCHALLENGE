80b77939dd0052c1c7654c1bc6b599a3ddc0778f
Rohan Mallick <rohanmallick016@gmail.com>
2024-03-11 15:43:19 +0530

    Add files via upload

---
+++ python/Custom Sort String.py
@@ -0,0 +1,38 @@
+from collections import Counter
+def customSortString(order, T):
+    cnt = Counter(T)  # count each char in T.
+    ans = [cnt.pop(c, 0) * c for c in order]  # sort chars both in T and order by the sequence of order.
+    for c, v in cnt.items():
+        ans.append(c * v)  # group chars in T but not in order.
+    return ''.join(ans)
+print(customSortString(  "cba","abcd" ))
+'''
+You are given two strings order and s. All the characters of order are unique and were sorted in some custom order previously.
+
+Permute the characters of s so that they match the order that order was sorted. More specifically, if a character x occurs before a character y in order, then x should occur before y in the permuted string.
+
+Return any permutation of s that satisfies this property.
+
+ 
+
+Example 1:
+
+Input:  order = "cba", s = "abcd" 
+
+Output:  "cbad" 
+
+Explanation: "a", "b", "c" appear in order, so the order of "a", "b", "c" should be "c", "b", and "a".
+
+Since "d" does not appear in order, it can be at any position in the returned string. "dcba", "cdba", "cbda" are also valid outputs.
+
+Example 2:
+
+Input:  order = "bcafg", s = "abcd" 
+
+Output:  "bcad" 
+
+Explanation: The characters "b", "c", and "a" from order dictate the order for the characters in s. The character "d" in s does not appear in order, so its position is flexible.
+
+Following the order of appearance in order, "b", "c", and "a" from s should be arranged as "b", "c", "a". "d" can be placed at any position since it's not in order. The output "bcad" correctly follows this rule. Other arrangements like "bacd" or "bcda" would also be valid, as long as "b", "c", "a" maintain their order.
+
+ '''
