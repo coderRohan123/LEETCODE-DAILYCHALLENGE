bebe6ba1d57014bea1ab8c0850d53105c1d90242
Rohan Mallick <rohanmallick016@gmail.com>
2024-04-06 11:53:27 +0530

    Add files via upload

---
+++ python/Minimum Remove to Make Valid Parentheses.py
@@ -0,0 +1,52 @@
+def minRemoveToMakeValid(self, s: str) -> str:
+    s = list(s)
+    stack = []
+    for i, char in enumerate(s):
+        if char == '(':
+            stack.append(i)
+        elif char == ')':
+            if stack:
+                stack.pop()
+            else:
+                s[i] = ''
+    while stack:
+        s[stack.pop()] = ''
+    return ''.join(s)
+print("ab(c)d")
+'''
+1249. Minimum Remove to Make Valid Parentheses
+Medium
+
+6321
+
+125
+
+Add to List
+
+Share
+Given a string s of '(' , ')' and lowercase English characters.
+
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+ 
+
+Example 1:
+
+Input: s = "lee(t(c)o)de)"
+Output: "lee(t(c)o)de"
+Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+Example 2:
+
+Input: s = "a)b(c)d"
+Output: "ab(c)d"
+Example 3:
+
+Input: s = "))(("
+Output: ""
+Explanation: An empty string is also valid.
+'''
