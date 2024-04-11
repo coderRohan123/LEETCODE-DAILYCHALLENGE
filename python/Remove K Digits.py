4fc8f19e7fb397b084b80a9d9ae7113bca5ebaa5
Rohan Mallick <rohanmallick016@gmail.com>
2024-04-11 10:38:02 +0530

    Add files via upload

---
+++ python/Remove K Digits.py
@@ -0,0 +1,31 @@
+def removeKdigits(num, k):
+    stack = []
+    for digit in num:
+        while k > 0 and len(stack) > 0 and stack[-1] > digit:
+            k -= 1
+            stack.pop()
+        stack.append(digit)
+    if k > 0:
+        stack = stack[:-k]
+    return "".join(stack).lstrip("0") or "0"
+print(removeKdigits("1432219", 3))
+'''
+Given string num representing a non-negative integer num, and an integer k, return the smallest possible integer after removing k digits from num.
+
+ 
+
+Example 1:
+
+Input: num = "1432219", k = 3
+Output: "1219"
+Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+Example 2:
+
+Input: num = "10200", k = 1
+Output: "200"
+Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+Example 3:
+
+Input: num = "10", k = 2
+Output: "0"
+Explanation: Remove all the digits from the number and it is left with nothing which is 0.'''
