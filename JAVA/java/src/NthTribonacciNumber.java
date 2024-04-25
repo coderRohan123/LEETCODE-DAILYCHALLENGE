6ae115bb0218067a251b669d5bf839279dbf61d6
Rohan Mallick <rohanmallick016@gmail.com>
2024-04-25 12:05:30 +0530

    Add files via upload

---
+++ JAVA/java/src/NthTribonacciNumber.java
@@ -0,0 +1,21 @@
+class NthTribonacciNumber {
+    public int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        int t0 = 0, t1 = 1, t2 = 1;
+        for (int i = 3; i <= n; i++) {
+            int tn = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = tn;
+        }
+        return t2;
+    }
+
+    public static void main(String[] args) {
+        NthTribonacciNumber solution = new NthTribonacciNumber();
+        System.out.println(solution.tribonacci(4)); // Output: 4
+    }
+}
+
