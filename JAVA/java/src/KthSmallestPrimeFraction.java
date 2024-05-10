ae1d406368aa3332b59d2abccbf9d2d528ca3e56
Rohan Mallick <rohanmallick016@gmail.com>
2024-05-10 11:33:30 +0530

    Add files via upload

---
+++ JAVA/java/src/KthSmallestPrimeFraction.java
@@ -0,0 +1,44 @@
+import java.util.PriorityQueue;
+
+public class KthSmallestPrimeFraction {
+    public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+        // Create a priority queue to store pairs of fractions
+        PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
+
+        // Push the fractions formed by dividing each element by
+        // the largest element into the priority queue
+        for (int i = 0; i < arr.length; i++) {
+            pq.offer(new double[]{
+                    -1.0 * arr[i] / arr[arr.length - 1],
+                    i,
+                    arr.length - 1
+            });
+        }
+
+        // Iteratively remove the top element (smallest fraction)
+        // and replace it with the next smallest fraction
+        while (--k > 0) {
+            double[] cur = pq.poll();
+            int numeratorIndex = (int) cur[1];
+            int denominatorIndex = (int) cur[2] - 1;
+            if (denominatorIndex > numeratorIndex) {
+                pq.offer(new double[]{
+                        -1.0 * arr[numeratorIndex] / arr[denominatorIndex],
+                        numeratorIndex,
+                        denominatorIndex
+                });
+            }
+        }
+
+        // Retrieve the kth smallest fraction from
+        // the top of the priority queue
+        double[] result = pq.poll();
+        return new int[]{arr[(int) result[1]], arr[(int) result[2]]};
+    }
+
+
+    public static void main(String[] args) {
+        KthSmallestPrimeFraction ob=new KthSmallestPrimeFraction();
+        System.out.println(ob.kthSmallestPrimeFraction(new int[] {1,7},1));
+    }
+}
