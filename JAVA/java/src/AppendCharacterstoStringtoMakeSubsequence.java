98abfcce79d124378fd0a447172d42bee472ac27
Rohan Mallick <rohanmallick016@gmail.com>
2024-06-03 09:14:22 +0530

    Add files via upload

---
+++ JAVA/java/src/AppendCharacterstoStringtoMakeSubsequence.java
@@ -0,0 +1,23 @@
+public class AppendCharacterstoStringtoMakeSubsequence {
+    public int appendCharacters(String s, String t) {
+        int first = 0, longestPrefix = 0;
+
+        while (first < s.length() && longestPrefix < t.length()) {
+            if (s.charAt(first) == t.charAt(longestPrefix)) {
+                // Since at the current position both the characters are equal,
+                // increment longestPrefix by 1
+                longestPrefix++;
+            }
+            first++;
+        }
+
+        // The number of characters appended is given by the difference in
+        // length of t and longestPrefix
+        return t.length() - longestPrefix;
+    }
+
+    public static void main(String[] args) {
+        AppendCharacterstoStringtoMakeSubsequence ob=new AppendCharacterstoStringtoMakeSubsequence();
+        System.out.println(ob.appendCharacters("coaching", "coding"));
+    }
+}
