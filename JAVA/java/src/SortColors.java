aeb7cfca22547cccb3d469a6dae34aad0b77ee22
Rohan Mallick <rohanmallick016@gmail.com>
2024-06-12 18:51:28 +0530

    Add files via upload

---
+++ JAVA/java/src/SortColors.java
@@ -0,0 +1,22 @@
+public class SortColors {
+    public static int[] sortColors(int[] nums) {
+        // 2-pass
+        int count0 = 0, count1 = 0, count2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {count0++;}
+            if (nums[i] == 1) {count1++;}
+            if (nums[i] == 2) {count2++;}
+        }
+        for(int i = 0; i < nums.length; i++) {
+            if (i < count0) {nums[i] = 0;}
+            else if (i < count0 + count1) {nums[i] = 1;}
+            else {nums[i] = 2;}
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sortColors(new int[] {2,0,2,1,1,0}));
+
+    }
+}
