aa2b386f2cbc8166d71624f054e2d26481514663
Rohan Mallick <rohanmallick016@gmail.com>
2024-06-17 14:59:46 +0530

    Add files via upload

---
+++ JAVA/java/src/SumofSquareNumbers.java
@@ -0,0 +1,22 @@
+public class SumofSquareNumbers {
+    public static boolean judgeSquareSum(int c) {
+        for (int i = 2; i * i <= c; i++) {
+            int count = 0;
+            if (c % i == 0) {
+                while (c % i == 0) {
+                    count++;
+                    c /= i;
+                }
+                if (i % 4 == 3 && count % 2 != 0)
+                    return false;
+            }
+        }
+        return c % 4 != 3;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(judgeSquareSum(5));
+
+    }
+
+}
