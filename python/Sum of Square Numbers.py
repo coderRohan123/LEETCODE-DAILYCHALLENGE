281e5116dde2c0e9dcb3bb45a24dfbb155460ea8
Rohan Mallick <rohanmallick016@gmail.com>
2024-06-17 15:00:20 +0530

    Add files via upload

---
+++ python/Sum of Square Numbers.py
@@ -0,0 +1,8 @@
+from math import sqrt
+def judgeSquareSum(c: int) -> bool:
+    for a in range(int(sqrt(c)) + 1):  # Iterate through all possible values of `a`
+        b = sqrt(c - a * a)  # Compute `b` as the square root of `c - a^2`
+        if b == int(b):  # Check if `b` is an integer
+            return True  # If `b` is an integer, return true
+    return False  # If no such pair `(a, b)` is found, return false
+print(judgeSquareSum(5))
