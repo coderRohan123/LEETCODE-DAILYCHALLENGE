db20f9505a3412444b1f2e01c968187074d5f9a8
Rohan Mallick <rohanmallick016@gmail.com>
2024-06-18 17:46:21 +0530

    Add files via upload

---
+++ JAVA/java/src/MostProfitAssigningWork.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MostProfitAssigningWork {
+
+    public static int maxProfitAssignment(
+            int[] difficulty,
+            int[] profit,
+            int[] worker
+    ) {
+        List<int[]> jobProfile = new ArrayList<>();
+        jobProfile.add(new int[] { 0, 0 });
+        for (int i = 0; i < difficulty.length; i++) {
+            jobProfile.add(new int[] { difficulty[i], profit[i] });
+        }
+
+        // Sort by difficulty values in increasing order.
+        Collections.sort(jobProfile, (a, b) -> Integer.compare(a[0], b[0]));
+        for (int i = 0; i < jobProfile.size() - 1; i++) {
+            jobProfile.get(i + 1)[1] = Math.max(
+                    jobProfile.get(i)[1],
+                    jobProfile.get(i + 1)[1]
+            );
+        }
+
+        int netProfit = 0;
+        for (int i = 0; i < worker.length; i++) {
+            int ability = worker[i];
+
+            // Find the job with just smaller or equal difficulty than ability.
+            int l = 0, r = jobProfile.size() - 1, jobProfit = 0;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (jobProfile.get(mid)[0] <= ability) {
+                    jobProfit = Math.max(jobProfit, jobProfile.get(mid)[1]);
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+
+            // Increment profit of current worker to total profit.
+            netProfit += jobProfit;
+        }
+        return netProfit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfitAssignment(new int[]{2,4,6,8,10}, new int[]{10,20,30,40,50}, new int[]{4,5,6,7}));
+
+    }
+}
