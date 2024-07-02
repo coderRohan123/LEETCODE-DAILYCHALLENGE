0e946c1e0046236496afb69f21ac00f9471efc6b
Rohan Mallick <rohanmallick016@gmail.com>
2024-07-02 18:16:10 +0530

    Add files via upload

---
+++ JAVA/java/src/IntersectionofTwoArraysII.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class IntersectionofTwoArraysII {
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        int l1 = nums1.length;
+        int l2 = nums2.length;
+        int i = 0, j = 0, k = 0;
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        while( i < l1 && j < l2)
+        {
+            if(nums1[i] < nums2[j])
+            {
+                i++;
+            }
+            else if(nums1[i] > nums2[j])
+            {
+                j++;
+            }
+            else
+            {
+                nums1[k++] = nums1[i++];
+                j++;
+            }
+        }
+        return Arrays.copyOfRange(nums1,0,k);
+    }
+
+    public static void main(String[] args) {
+        int b[]=intersect(new int[] {1,2,2,1}, new int[] {2,2});
+        System.out.println(Arrays.toString(b));
+    }
+}
