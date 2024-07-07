bfe1492d2fca52c9144c18980c7e76b3998eb900
Rohan Mallick <rohanmallick016@gmail.com>
2024-07-07 10:09:15 +0530

    Add files via upload

---
+++ python/Water Bottles.py
@@ -0,0 +1,15 @@
+def numWaterBottles(numBottles: int, numExchange: int) -> int:
+    consumed_bottles = 0
+
+    while numBottles >= numExchange:
+        # Maximum number of times we can consume numExchange
+        # number of bottles using numBottles.
+        K = numBottles // numExchange
+
+        consumed_bottles += numExchange * K
+        numBottles -= numExchange * K
+
+        numBottles += K
+
+    return consumed_bottles + numBottles
+print(numWaterBottles(9,3))
